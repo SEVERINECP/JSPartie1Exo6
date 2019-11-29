161876eff24bf101a91bd09d168554930a9c5238
SEVERINECP <sevpicot@gmail.com>
2019-11-29 11:43:54 +0100

    JSPartie1Exo7Corrige

---
+++ assets/JS/sript.js
@@ -0,0 +1,7 @@
+function information(){
+var yourAge = document.getElementById('yourAge').value;
+if (yourAge>=18) {
+  alert('Vous êtes majeur');
+}else (yourAge<18){
+  alert('Vous êtes mineur');
+}
